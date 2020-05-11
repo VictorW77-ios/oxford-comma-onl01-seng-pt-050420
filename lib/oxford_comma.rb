f918fb4299612d15d0a05bcc3f1129db2b93d736
Victor Williams <victor.williams1030@gmail.com>
2020-05-11 20:55:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  array.length == 1?
+  if array.length == 1
     array.join
-  array.length == 2?
+  elsif array.length == 2
     array.join( "and" )
   array[0..-2].join(" , ") + ", and " + array[-1]
   end
